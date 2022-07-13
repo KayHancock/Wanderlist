63cc75b0fee8fff46663aa5ccea81edca5da09a0
Peanut Butter <avni0000patel@gmail.com>
2022-07-12 20:01:19 -0400

    delete currency & restaurants api

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 // Variables 
 
-
 // API Fetch Requests
-// Fixer API (currency exchange)
-var myHeaders = new Headers();
-myHeaders.append("apikey", "kSJyqbTcJpl631qdUTIm4KBNnsmZDdE0");
-
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-  headers: myHeaders
-};
-
-fetch("https://api.apilayer.com/fixer/convert?to=EUR&from=USD&amount=100", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
 
 // Hotel API
 var options = {
@@ -27,28 +12,6 @@ var options = {
 };
 
 fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york&locale=en_US&currency=USD', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-// Worldwide Restaurants API
-var encodedParams = new URLSearchParams();
-encodedParams.append("language", "en_US");
-encodedParams.append("limit", "30");
-encodedParams.append("location_id", "297704");
-encodedParams.append("currency", "USD");
-
-var restaurantOptions = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'b9572fd035msh13565d5f0f8a81bp1f763bjsn7e7b5a735ea3',
-		'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-	},
-	body: encodedParams
-};
-
-fetch('https://worldwide-restaurants.p.rapidapi.com/search', restaurantOptions)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
