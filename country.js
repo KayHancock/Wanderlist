b56cd4f59c9f789b1c221c32cbf2399ca5d640bd
Peanut Butter <avni0000patel@gmail.com>
2022-07-18 22:31:36 -0400

    added country css and some travelbreifing info

--- country.js
+++ country.js
@@ -1,7 +1,19 @@
 var countryNameEl = document.querySelector("#countryName");
 
+var drinkingWaterDescription = document.querySelector("#drinkingWaterDescription");
+console.log(drinkingWaterDescription);
 var drinkingWaterInfo = document.querySelector("#drinkingWaterInfo");
 console.log(drinkingWaterInfo);
+
+var timezoneDescription = document.querySelector("#timezoneDescription");
+console.log(timezoneDescription);
+var timezoneInfo = document.querySelector("#timezoneInfo");
+console.log(timezoneInfo);
+
+var currencyDescription = document.querySelector("#currencyDescription");
+console.log(currencyDescription);
+var currencyInfo = document.querySelector("#currencyInfo");
+console.log(currencyInfo);
 
 var getCountryInfo = async (countryName) => {
     var queryString = document.location.search;
@@ -18,15 +30,107 @@ var getCountryInfo = async (countryName) => {
 			return response.json();
 		})
 		.then(function (response) {
+			// Travel Advise
+			console.log(response);
+			console.log(response.advise);
+			console.log(response.advise.CA);
+			console.log(response.advise.UA);
+			console.log(response.advise.CA.advise);
+			console.log(response.advise.UA.advise);
+			console.log(response.advise.CA.url);
+			console.log(response.advise.UA.url);
+
+
+			var adviseDescription = document.createElement("div");
+			adviseDescription.innerHTML = "Travel Advise";
+			console.log(adviseDescription.innerHTML);
+			travelAdviseDescription.appendChild(adviseDescription);
+
+			var adviseCA = document.createElement("div");
+			console.log(response.advise.CA.advise);
+			adviseCA.innerHTML = "Canadian government: " + response.advise.CA.advise + " in " + countryName;
+			console.log(adviseCA.innerHTML);
+			travelAdviseInfo.appendChild(adviseCA);
+
+			// Create anchor element.
+			var urlCA = document.createElement('a'); 
+			// Create the text node for anchor element.
+			var link = document.createTextNode("Full Report from Canadian Government");
+			// Append the text node to anchor element.
+			urlCA.appendChild(link); 
+			// Set the title.
+			urlCA.title = "Full Report from Canadian Government";
+			// Console log response.advise.CA.url
+			console.log(response.advise.CA.url); 
+			// Set the href property.
+			urlCA.href = response.advise.CA.url;
+			// Append the anchor element to the body.
+			travelAdviseInfo.appendChild(urlCA); 
+
+			var adviseUA = document.createElement("div");
+			adviseUA.innerHTML = "Austrailian government: " + response.advise.UA.advise + " in " + countryName;
+			console.log(adviseUA.innerHTML);
+			travelAdviseInfo.appendChild(adviseUA);
+
+			// Create anchor element.
+			var urlUA = document.createElement('a'); 
+			// Create the text node for anchor element.
+			var link = document.createTextNode("Full Report from Australian Government");
+			// Append the text node to anchor element.
+			urlUA.appendChild(link); 
+			// Set the title.
+			urlUA.title = "Full Report from Australian Government";
+			// Console log response.advise.CA.url
+			console.log(response.advise.UA.url); 
+			// Set the href property.
+			urlUA.href = response.advise.UA.url;
+			// Append the anchor element to the body.
+			travelAdviseInfo.appendChild(urlUA);
+
+			// Drinking water
 			console.log(response);
 			console.log(response.water);
 			console.log(response.water.short);
 
+			var waterDescription = document.createElement("div");
+			waterDescription.innerHTML = "Drinking Water";
+			console.log(waterDescription.innerHTML);
+			drinkingWaterDescription.appendChild(waterDescription);
 			var water = document.createElement("div");
 			console.log(response.water.short);
-			water.innerHTML = "Drinking water in " + countryName + " is " + response.water.short + ".";
+			water.innerHTML = "Drinking tap water in " + countryName + " is " + response.water.short;
 			console.log(water.innerHTML);
 			drinkingWaterInfo.appendChild(water);
+
+			// Timezone
+			console.log(response);
+			console.log(response.timezone);
+			console.log(response.timezone.name);
+
+			var timeDescription = document.createElement("div");
+			timeDescription.innerHTML = "Timezone";
+			console.log(timeDescription.innerHTML);
+			timezoneDescription.appendChild(timeDescription);
+			var time = document.createElement("div");
+			console.log(response.timezone.name);
+			time.innerHTML = response.timezone.name;
+			console.log(time.innerHTML);
+			timezoneInfo.appendChild(time);
+
+			// Currency Converter
+			console.log(response);
+			console.log(response.currency);
+			console.log(response.currency.code);
+
+			var currDescription = document.createElement("div");
+			currDescription.innerHTML = "Currency Converter";
+			console.log(currDescription.innerHTML);
+			currencyDescription.appendChild(currDescription);
+			var currency = document.createElement("div");
+			console.log(response.currency.code);
+			currency.innerHTML = "The currency in " + countryName + " is " + response.currency.code + ".";
+			console.log(currency.innerHTML);
+			currencyInfo.appendChild(currency);
 
 			// call map
 			const latitude = response.maps.lat;
