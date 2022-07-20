18335691a252becb388da99b3747546cbf20b315
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-07-19 20:18:01 -0400

    removed hotel api from script.js

--- script.js
+++ script.js
@@ -15,22 +15,6 @@ var searchBtn = document.getElementById("search");
 var countryRef = document.querySelector("#countryRef");
 
 // API Fetch Requests
-
-// Hotel API
-var options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b9572fd035msh13565d5f0f8a81bp1f763bjsn7e7b5a735ea3',
-		'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-	}
-};
-
-fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york&locale=en_US&currency=USD', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
 // Travel-Breifing on page
 function countrySearch() {
 	$("#country-anchor").remove();
