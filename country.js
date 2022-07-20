8c9ea7717338eba14853d2f3c83ea8c944a4ad05
Peanut Butter <avni0000patel@gmail.com>
2022-07-20 00:24:32 -0400

    if null or  or length = 0

--- country.js
+++ country.js
@@ -25,12 +25,6 @@ console.log(timezoneDescription);
 var timezoneInfo = document.querySelector("#timezoneInfo");
 console.log(timezoneInfo);
 
-// Visa Requirements
-var visaRequirementsDescription = document.querySelector("#visaRequirementsDescription");
-console.log(visaRequirementsDescription);
-var visaRequirementsInfo = document.querySelector("#visaRequirementsInfo");
-console.log(visaRequirementsInfo);
-
 // Currency
 var currencyDescription = document.querySelector("#currencyDescription");
 console.log(currencyDescription);
@@ -42,12 +36,6 @@ var languageDescription = document.querySelector("#languageDescription");
 console.log(languageDescription);
 var languageInfo = document.querySelector("#languageInfo");
 console.log(languageInfo);
-
-// Weather
-var weatherDescription = document.querySelector("#weatherDescription");
-console.log(weatherDescription);
-var weatherInfo = document.querySelector("#weatherInfo");
-console.log(weatherInfo);
 
 // Vaccinations and Health
 var vaccinationsHealthDescription = document.querySelector("#vaccinationsHealthDescription");
@@ -82,14 +70,14 @@ var travelPage = 'travel.html';
 var surpriseBtn = document.getElementById("surprise");
 
 var getCountryInfo = async (countryName) => {
-    var queryString = document.location.search;
-    var countryName = queryString.split('=')[1];
+	var queryString = document.location.search;
+	var countryName = queryString.split('=')[1];
 
-    if (countryName) {
-        countryNameEl.textContent = countryName;
-        console.log(countryNameEl.textContent);
-        console.log(countryName);
-    }
+	if (countryName) {
+		countryNameEl.textContent = countryName;
+		console.log(countryNameEl.textContent);
+		console.log(countryName);
+	}
 
 	return fetch(`https://travelbriefing.org/${countryName}?format=json`)
 		.then(function (response) {
@@ -106,23 +94,22 @@ var getCountryInfo = async (countryName) => {
 			console.log(neighborDescription.innerHTML);
 			neighboringCountryDescription.appendChild(neighborDescription);
 
-			// Neighboring countries info if no data found
+			// Neighboring countries info
 			if (response.neighbors.length === 0) {
 				var neighbor = document.createElement("div");
 				neighbor.className = "neighbor";
 				neighbor.innerHTML = "No data found";
 				console.log(neighbor.innerHTML);
 				languageInfo.appendChild(neighbor);
-			}
-
-			// Neighboring countries info
-			for (i = 0; i < response.neighbors.length; i++ ) {
-				console.log(response.neighbors[i].name);
-				var neighbor = document.createElement("div");
-				neighbor.className = "neighbor";
-				neighbor.innerHTML = response.neighbors[i].name;
-				console.log(neighbor.innerHTML);
-				neighboringCountryInfo.appendChild(neighbor);
+			} else {
+				for (i = 0; i < response.neighbors.length; i++) {
+					console.log(response.neighbors[i].name);
+					var neighbor = document.createElement("div");
+					neighbor.className = "neighbor";
+					neighbor.innerHTML = response.neighbors[i].name;
+					console.log(neighbor.innerHTML);
+					neighboringCountryInfo.appendChild(neighbor);
+				}
 			}
 
 			// Travel advise
@@ -134,6 +121,15 @@ var getCountryInfo = async (countryName) => {
 			adviseDescription.innerHTML = "Travel Advise";
 			console.log(adviseDescription.innerHTML);
 			travelAdviseDescription.appendChild(adviseDescription);
+
+			if (response.advise.length === 0) {
+				var advise = document.createElement("div");
+				advise.className = "advise";
+				console.log(response.advise);
+				advise.innerHTML = "No data found";
+				console.log(advise.innerHTML);
+				travelAdviseInfo.appendChild(advise);
+			}
 
 			// Canadian government travel advise summary
 			if (response.advise.CA) {
@@ -148,18 +144,18 @@ var getCountryInfo = async (countryName) => {
 				// Create anchor element
 				var urlCA = document.createElement('a');
 				// Add class name to anchor element
-				urlCA.className = "urlCA"; 
+				urlCA.className = "urlCA";
 				// Create the text node for anchor element
 				var link = document.createTextNode("Full Report from Canadian Government");
 				// Append the text node to anchor element
-				urlCA.appendChild(link); 
+				urlCA.appendChild(link);
 				// Set the title
 				urlCA.title = "Full Report from Canadian Government";
 				// Set the href property
 				urlCA.href = response.advise.CA.url;
 				// Append the anchor element to the body
 				travelAdviseInfo.appendChild(urlCA);
-			} 
+			}
 
 			// Australian government travel advise summary
 			if (response.advise.UA) {
@@ -173,11 +169,11 @@ var getCountryInfo = async (countryName) => {
 				// Create anchor element.
 				var urlUA = document.createElement('a');
 				// Add class name to anchor element
-				urlUA.className = "urlUA";  
+				urlUA.className = "urlUA";
 				// Create the text node for anchor element.
 				var link = document.createTextNode("Full Report from Australian Government");
 				// Append the text node to anchor element.
-				urlUA.appendChild(link); 
+				urlUA.appendChild(link);
 				// Set the title.
 				urlUA.title = "Full Report from Australian Government";
 				// Set the href property.
@@ -198,13 +194,19 @@ var getCountryInfo = async (countryName) => {
 			drinkingWaterDescription.appendChild(waterDescription);
 
 			// Drinking water info
-			var water = document.createElement("div");
-			water.className = "water"; 
-			water.innerHTML = "Drinking tap water in " + countryName + " is " + response.water.short;
-			console.log(water.innerHTML);
-			drinkingWaterInfo.appendChild(water);
-			
-			// Visa requirements
+			if (response.water.short === null) {
+				var water = document.createElement("div");
+				water.className = "water";
+				water.innerHTML = "No data found";
+				console.log(water.innerHTML);
+				drinkingWaterInfo.appendChild(water);
+			} else {
+				var water = document.createElement("div");
+				water.className = "water";
+				water.innerHTML = "Drinking tap water in " + countryName + " is " + response.water.short;
+				console.log(water.innerHTML);
+				drinkingWaterInfo.appendChild(water);
+			}
 
 			// Timezone
 			console.log(response);
@@ -218,16 +220,25 @@ var getCountryInfo = async (countryName) => {
 			timezoneDescription.appendChild(timeDescription);
 
 			// Timezone info
-			var time = document.createElement("div");
-			time.className = "time";
-			time.innerHTML = "The time zone in " + countryName + " is " + response.timezone.name;
-			console.log(time.innerHTML);
-			timezoneInfo.appendChild(time);
+			if (response.timezone.name === null) {
+				var time = document.createElement("div");
+				time.className = "time";
+				time.innerHTML = "No data found";
+				console.log(time.innerHTML);
+				timezoneInfo.appendChild(time);
+			} else {
+				var time = document.createElement("div");
+				time.className = "time";
+				time.innerHTML = "The time zone in " + countryName + " is " + response.timezone.name;
+				console.log(time.innerHTML);
+				timezoneInfo.appendChild(time);
+			}
 
 			// Currency
 			console.log(response);
 			console.log(response.currency);
-			console.log(response.currency.code);
+			console.log(response.currency.name);
+			console.log(response.currency.symbol);
 
 			// Currency description
 			var currDescription = document.createElement("div");
@@ -236,11 +247,31 @@ var getCountryInfo = async (countryName) => {
 			currencyDescription.appendChild(currDescription);
 
 			// Currency info
-			var curr = document.createElement("div");
-			curr.className = "curr";
-			curr.innerHTML = "The currency in " + countryName + " is " + response.currency.code;
-			console.log(curr.innerHTML);
-			currencyInfo.appendChild(curr);
+			if (response.currency.name === null && response.currency.symbol === null) {
+				var curr = document.createElement("div");
+				curr.className = "curr";
+				curr.innerHTML = "No data found";
+				console.log(curr.innerHTML);
+				currencyInfo.appendChild(curr);
+			} else if (response.currency.name === null) {
+				var curr = document.createElement("div");
+				curr.className = "curr";
+				curr.innerHTML = "The currency in " + countryName + " is " + "(" + response.currency.symbol + ")";
+				console.log(curr.innerHTML);
+				currencyInfo.appendChild(curr);
+			} else if (response.currency.symbol === null) {
+				var curr = document.createElement("div");
+				curr.className = "curr";
+				curr.innerHTML = "The currency in " + countryName + " is " + response.currency.name;
+				console.log(curr.innerHTML);
+				currencyInfo.appendChild(curr);
+			} else {
+				var curr = document.createElement("div");
+				curr.className = "curr";
+				curr.innerHTML = "The currency in " + countryName + " is " + response.currency.name + " (" + response.currency.symbol + ")";
+				console.log(curr.innerHTML);
+				currencyInfo.appendChild(curr);
+			}
 
 			// Language
 			console.log(response);
@@ -252,26 +283,23 @@ var getCountryInfo = async (countryName) => {
 			console.log(langDescription.innerHTML);
 			languageDescription.appendChild(langDescription);
 
-			// Language info if no data found
+			// Language info
 			if (response.language.length === 0) {
 				var lang = document.createElement("div");
 				lang.className = "lang";
 				lang.innerHTML = "No data found";
 				console.log(lang.innerHTML);
 				languageInfo.appendChild(lang);
-			} 
-
-			// Language info
-			for (i = 0; i < response.language.length; i++ ) {
-				console.log(response.language[i].language);
-				var lang = document.createElement("div");
-				lang.className = "lang";
-				lang.innerHTML = response.language[i].language;
-				console.log(lang.innerHTML);
-				languageInfo.appendChild(lang);
+			} else {
+				for (i = 0; i < response.language.length; i++) {
+					console.log(response.language[i].language);
+					var lang = document.createElement("div");
+					lang.className = "lang";
+					lang.innerHTML = response.language[i].language;
+					console.log(lang.innerHTML);
+					languageInfo.appendChild(lang);
+				}
 			}
-
-			// Weather
 
 			// Vaccinations and health
 			console.log(response);
@@ -283,29 +311,28 @@ var getCountryInfo = async (countryName) => {
 			console.log(healthDescription.innerHTML);
 			vaccinationsHealthDescription.appendChild(healthDescription);
 
-			// Language info if no data found
+			// Vaccinations and health info
 			if (response.vaccinations.length === 0) {
 				var healthMessage = document.createElement("div");
 				healthMessage.className = "healthMessage";
 				healthMessage.innerHTML = "No data found";
 				console.log(healthMessage.innerHTML);
 				vaccinationsHealthInfo.appendChild(healthMessage);
-			}
-
-			// Vaccinations and health info
-			for (i = 0; i < response.vaccinations.length; i++ ) {
-				console.log(response.vaccinations[i].name);
-				var healthName = document.createElement("div");
-				healthName.className = "healthName";
-				healthName.innerHTML = response.vaccinations[i].name;
-				console.log(healthName.innerHTML);
-				vaccinationsHealthInfo.appendChild(healthName);
-				console.log(response.vaccinations[i].message);
-				var healthMessage = document.createElement("div");
-				healthMessage.className = "healthMessage";
-				healthMessage.innerHTML = response.vaccinations[i].message;
-				console.log(healthMessage.innerHTML);
-				vaccinationsHealthInfo.appendChild(healthMessage);
+			} else {
+				for (i = 0; i < response.vaccinations.length; i++) {
+					console.log(response.vaccinations[i].name);
+					var healthName = document.createElement("div");
+					healthName.className = "healthName";
+					healthName.innerHTML = response.vaccinations[i].name;
+					console.log(healthName.innerHTML);
+					vaccinationsHealthInfo.appendChild(healthName);
+					console.log(response.vaccinations[i].message);
+					var healthMessage = document.createElement("div");
+					healthMessage.className = "healthMessage";
+					healthMessage.innerHTML = response.vaccinations[i].message;
+					console.log(healthMessage.innerHTML);
+					vaccinationsHealthInfo.appendChild(healthMessage);
+				}
 			}
 
 			// Telephone
@@ -321,34 +348,66 @@ var getCountryInfo = async (countryName) => {
 			phoneDescription.innerHTML = "Telephone";
 			console.log(phoneDescription.innerHTML);
 			telephoneDescription.appendChild(phoneDescription);
-			
+
 			// Calling code telephone info
-			var callingCode = document.createElement("div");
-			callingCode.className = "callingCode";
-			callingCode.innerHTML = " 📞 " + response.telephone.calling_code;
-			console.log(callingCode.innerHTML);
-			telephoneInfo.appendChild(callingCode);
+			if (response.telephone.calling_code === null) {
+				var callingCode = document.createElement("div");
+				callingCode.className = "callingCode";
+				callingCode.innerHTML = " 📞 " + "No data found";
+				console.log(callingCode.innerHTML);
+				telephoneInfo.appendChild(callingCode);
+			} else {
+				var callingCode = document.createElement("div");
+				callingCode.className = "callingCode";
+				callingCode.innerHTML = " 📞 " + response.telephone.calling_code;
+				console.log(callingCode.innerHTML);
+				telephoneInfo.appendChild(callingCode);
+			}
 
 			// Ambulance telephone info
-			var ambulance = document.createElement("div");
-			ambulance.className = "ambulance";
-			ambulance.innerHTML = "🚑 " + response.telephone.ambulance;
-			console.log(ambulance.innerHTML);
-			telephoneInfo.appendChild(ambulance);
+			if (response.telephone.ambulance === "") {
+				var ambulance = document.createElement("div");
+				ambulance.className = "ambulance";
+				ambulance.innerHTML = "🚑 " + "No data found";
+				console.log(ambulance.innerHTML);
+				telephoneInfo.appendChild(ambulance);
+			} else {
+				var ambulance = document.createElement("div");
+				ambulance.className = "ambulance";
+				ambulance.innerHTML = "🚑 " + response.telephone.ambulance;
+				console.log(ambulance.innerHTML);
+				telephoneInfo.appendChild(ambulance);
+			}
 
 			// Fire telephone info
-			var fire = document.createElement("div");
-			fire.className = "fire";
-			fire.innerHTML = "🔥 " + response.telephone.fire;
-			console.log(fire.innerHTML);
-			telephoneInfo.appendChild(fire);
+			if (response.telephone.fire === "") {
+				var fire = document.createElement("div");
+				fire.className = "fire";
+				fire.innerHTML = "🔥 " + "No data found";
+				console.log(fire.innerHTML);
+				telephoneInfo.appendChild(fire);
+			} else {
+				var fire = document.createElement("div");
+				fire.className = "fire";
+				fire.innerHTML = "🔥 " + response.telephone.fire;
+				console.log(fire.innerHTML);
+				telephoneInfo.appendChild(fire);
+			}
 
 			// Police telephone info
-			var police = document.createElement("div");
-			police.className = "police";
-			police.innerHTML = "🚓 " + response.telephone.police;
-			console.log(police.innerHTML);
-			telephoneInfo.appendChild(police);
+			if (response.telephone.police === "") {
+				var police = document.createElement("div");
+				police.className = "police";
+				police.innerHTML = "🚓 " + "No data found";
+				console.log(police.innerHTML);
+				telephoneInfo.appendChild(police);
+			} else {
+				var police = document.createElement("div");
+				police.className = "police";
+				police.innerHTML = "🚓 " + response.telephone.police;
+				console.log(police.innerHTML);
+				telephoneInfo.appendChild(police);
+			}
 
 			// Electricity
 			console.log(response);
@@ -364,37 +423,61 @@ var getCountryInfo = async (countryName) => {
 			electricityDescription.appendChild(elecDescription);
 
 			// Voltage electricity info
-			var voltage = document.createElement("div");
-			voltage.className = "voltage";
-			voltage.innerHTML = "Voltage: " + response.electricity.voltage;
-			console.log(voltage.innerHTML);
-			electricityInfo.appendChild(voltage);
+			if (response.electricity.voltage === "") {
+				var voltage = document.createElement("div");
+				voltage.className = "voltage";
+				voltage.innerHTML = "Voltage: No data found";
+				console.log(voltage.innerHTML);
+				electricityInfo.appendChild(voltage);
+			} else {
+				var voltage = document.createElement("div");
+				voltage.className = "voltage";
+				voltage.innerHTML = "Voltage: " + response.electricity.voltage;
+				console.log(voltage.innerHTML);
+				electricityInfo.appendChild(voltage);
+			}
 
 			// Frequency electricity info
-			var frequency = document.createElement("div");
-			frequency.className = "frequency";
-			frequency.innerHTML = "Frequency: " + response.electricity.frequency;
-			console.log(frequency.innerHTML);
-			electricityInfo.appendChild(frequency);
+			if (response.electricity.frequency === "") {
+				var frequency = document.createElement("div");
+				frequency.className = "frequency";
+				frequency.innerHTML = "Frequency: No data found";
+				console.log(frequency.innerHTML);
+				electricityInfo.appendChild(frequency);
+			} else {
+				var frequency = document.createElement("div");
+				frequency.className = "frequency";
+				frequency.innerHTML = "Frequency: " + response.electricity.frequency;
+				console.log(frequency.innerHTML);
+				electricityInfo.appendChild(frequency);
+			}
 
 			// Plugs electricity info
-			var plugsResponse = response.electricity.plugs;
-			plugsResponse.forEach(display)
-			function display(item) {
+			if (response.electricity.plugs.length === 0) {
+				console.log("Empty");
 				var plug = document.createElement("div");
 				plug.className = "plug";
-				plug.innerHTML += "Plug type: " + item + "<br>";
+				plug.innerHTML += "Plug type: No data found";
 				electricityInfo.appendChild(plug);
+			} else {
+				var plugsResponse = response.electricity.plugs;
+				plugsResponse.forEach(display)
+				function display(item) {
+					var plug = document.createElement("div");
+					plug.className = "plug";
+					plug.innerHTML += "Plug type: " + item + "<br>";
+					electricityInfo.appendChild(plug);
+				}
 			}
 
 			// call map
 			const latitude = response.maps.lat;
 			const longitude = response.maps.long;
 			const zoom = response.maps.zoom;
-			
+
 			setMap(latitude, longitude, zoom);
 		})
-		
+
 };
 
 getCountryInfo();
@@ -403,10 +486,10 @@ getCountryInfo();
 var map;
 
 function initMap(lat = 39.952583, long = -75.165222, zoom = 8) {
-	console.log(lat, long, zoom) 
+	console.log(lat, long, zoom)
 	map = new google.maps.Map(document.getElementById("map"), {
-	  center: { lat: lat, lng: long },
-	  zoom: zoom,
+		center: { lat: lat, lng: long },
+		zoom: zoom,
 	});
 }
 
@@ -414,13 +497,13 @@ function setMap(lat = 39.952583, long = -75.165222, zoom = 8) {
 	map.setCenter(new google.maps.LatLng(lat, long));
 	map.setZoom(Number(zoom));
 }
-  
+
 window.initMap = initMap;
 
 //end of google map
 
 // generate random country when user clicks 'surprise me'
-$("#surprise").click(function() {
+$("#surprise").click(function () {
 	var countryNames = [];
 	$("#country-anchor").remove();
 	$("#alert-message").remove();
@@ -434,18 +517,18 @@ $("#surprise").click(function() {
 				countryNames.push(data[i].name);
 				var randomIndex = Math.floor(Math.random() * countryNames.length);
 				document.location.replace('./country.html?country=' + countryNames[randomIndex]);
-				}
-	})
+			}
+		})
 })
 
-$("#home").click(function() {
+$("#home").click(function () {
 	document.location.replace(homePage);
 })
 
-$("#docs").click(function() {
+$("#docs").click(function () {
 	document.location.replace(docPage);
 })
 
-$("#browse").click(function() {
+$("#browse").click(function () {
 	document.location.replace(browsePage);
 })
